1b35e49e3f116d88a57792f70482a7e396e5803c
Sultanov Sanzhar <sulsanzhar@mail.ru>
2024-04-28 06:17:06 +0500

    some fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import './App.sass'
 import { Header } from '../Header/Header.jsx'
 import { Promo } from '../Promo/Promo.jsx'
 import { Education } from '../Education/Education.jsx'
 import { Skills } from '../Skills/Skills.jsx'
 import { Projects } from '../Projects/Projects.jsx'
+import './App.sass'
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <Header/>
       <Promo/>
       <Education/>
       <Skills/>
       <Projects/>
-    </>
+    </div>
   )
 }
 
